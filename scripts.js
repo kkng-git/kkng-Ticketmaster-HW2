7c481e71e2d0242f26ba819204c073df1904d871
kkng-git <ng.kendrick@yahoo.com>
2025-10-05 14:13:57 -0700

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+// Exports a simple hello world function for Ticketmaster_HW2
+
+function helloWorld() {
+  return 'Hello, world! — from Ticketmaster_HW2/scripts.js';
+}
+
+// window.helloWorld = helloWorld;
